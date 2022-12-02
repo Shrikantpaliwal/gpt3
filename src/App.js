77f18ffabf1e99f3ab9f2e6e054f63614e1ed9f8
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-12-03 01:35:36 +0530

    Root css added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header, Footer, Blog, Possiblity, Features, WhatGPT3 } from "./containers";
 import { CTA, Branding, Navbar } from './components';
+import './app.css';
 
 const App = () => {
     return (
