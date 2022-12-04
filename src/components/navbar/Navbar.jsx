545d078d57069d6e93dc769544c8c5c20fd9ddbd
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-12-04 16:37:20 +0530

    Navbar component added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 //BEM - block element modifier
+const MenuLinks = () => (<>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#home">What is GPT3?</a></p>
+  <p><a href="#home">Open AI</a></p>
+  <p><a href="#home">Case Studies</a></p>
+  <p><a href="#home">Library</a></p>
+</>)
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={ logo } alt="Logo" />
+          <img src={logo} alt="Logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#home">What is GPT3?</a></p>
-          <p><a href="#home">Open AI</a></p>
-          <p><a href="#home">Case Studies</a></p>
-          <p><a href="#home">Library</a></p>
+          <MenuLinks />
+        </div>
+        <div className="gpt3__navbar-sign">
+          <p>Sign in</p>
+          <button type="button">Sign Up</button>
         </div>
       </div>
+      <div className="gpt3__navbar-menu">
+          {toggleMenu ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} /> :
+            <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                <MenuLinks />
+              </div>
+              <div className="gpt3__navbar-menu_container-links-sign">
+                  <p>Sign in</p>
+                  <button type="button">Sign Up</button>
+                </div>
+            </div>
+          )}
+        </div>
     </div>
   )
 }
